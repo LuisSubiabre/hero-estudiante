dff90de66294d709735d60cf976cade481994d52
Luis <luis.subiabre@gmail.com>
2025-02-23 23:50:01 -0300

    chro: https

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-})
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://149.50.146.106:3500", // Tu API sin HTTPS
+        changeOrigin: true,
+        secure: false, // Permite HTTP en lugar de HTTPS
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
