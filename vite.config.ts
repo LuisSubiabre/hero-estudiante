c587382c9042fc7fdf75637200f28b387dccfb6d
Luis <luis.subiabre@gmail.com>
2025-02-24 10:33:54 -0300

    chore: vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,4 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://149.50.146.106:3500", // Tu servidor HTTP
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""), // Elimina el prefijo /api
-      },
-    },
-  },
 });
