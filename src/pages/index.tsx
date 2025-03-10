7c25127258f6eec70dcfe6216a52a5c92647fe8f
LuisSubiabre <luis.subiabre@gmail.com>
2025-03-10 09:36:00 -0300

    chore: footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,4 @@
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
-
-import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
 
 export default function IndexPage() {
@@ -22,7 +15,7 @@ export default function IndexPage() {
             La verdad a través de la razón
           </div>
         </div>
-
+        {/* 
         <div className="flex gap-3">
           <Link
             isExternal
@@ -43,8 +36,8 @@ export default function IndexPage() {
             <GithubIcon size={20} />
             GitHub
           </Link>
-        </div>
-
+        </div> */}
+        {/* 
         <div className="mt-8">
           <Snippet hideCopyButton hideSymbol variant="bordered">
             <span>
@@ -52,7 +45,7 @@ export default function IndexPage() {
               <Code color="primary">pages/index.tsx</Code>
             </span>
           </Snippet>
-        </div>
+        </div> */}
       </section>
     </DefaultLayout>
   );
